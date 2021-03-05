524756787fd07f1598c69e9be72a54b81308de81
山本翼 <yamamototsubasa@yamamototsubasanoMacBook-Pro.local>
2021-03-05 15:45:48 +0900

    tesut

---
+++ TesutGitAPP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TesutGitAPP
+//
+//  Created by 山本翼 on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("TesutGitAPP")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
